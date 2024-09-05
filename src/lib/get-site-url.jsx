0c9edd4b45997563b4938adb2c5221320adb8fd4
pawan <pawan.irish@gmail.com>
2024-09-05 22:51:28 +0530

    sdsdf

--- src/lib/get-site-url.jsx
+++ src/lib/get-site-url.jsx
@@ -7,7 +7,7 @@ export function getSiteURL() {
   'https://shabby-backend.onrender.com'
   // 'http://localhost:7000'
   // Make sure to include `https://` when not localhost.
-  url = url.includes('http') ? url : `https://shabby-backend.vercel.app`;
+  url = url.includes('http') ? url : `https://shabby-backend.onrender.com`;
   // Make sure to include a trailing `/`.
   url = url.endsWith('/') ? url : `${url}/`;
   return url;
